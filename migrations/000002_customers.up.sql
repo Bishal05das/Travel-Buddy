4439994ff91de44ea2e870cfbaf1eb4f36f94526
bishal <bishal05das@gmail.com>
2026-02-10 19:11:30 +0600

    update customer table

--- migrations/000002_customers.up.sql
+++ migrations/000002_customers.up.sql
@@ -1,9 +1,14 @@
 CREATE TABLE IF NOT EXISTS customers (
     customer_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID REFERENCES users(user_id) NULL,
-    name VARCHAR(100) NOT NULL, 
+    name VARCHAR(100), 
     email VARCHAR(150), 
     phone VARCHAR(20), 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CHECK (
+        (user_id IS NULL AND name IS NOT NULL AND email IS NOT NULL AND phone IS NOT NULL)
+        OR
+        (user_id IS NOT NULL AND name IS NULL AND email IS NULL AND phone IS NULL)
+    )
 );
